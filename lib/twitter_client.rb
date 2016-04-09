517d89319080d45f14551ac89f2ab8a8fb5efde0
Wen Yu <wongwy93@gmail.com>
2016-04-09 11:52:32 +0800

    Move method to private

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -1,43 +1,45 @@
 class TwitterClient
-	
-	def initialize
-		@client = twitter_credentials
-	end
+  
+  def initialize
+    @client = twitter_credentials
+  end
 
-	def post_tweet(result)
-		coordinate(result)
-		@client.update("Visitor from #{result}: The weather is #{@forecast.summary} and #{@forecast.temperature} celsius")
-	end
+  def post_tweet(ip_address)
+    coordinate(ip_address)
+    @client.update("Visitor from #{ip_address}: The weather is #{@forecast.summary} and #{@forecast.temperature} celsius")
+  end
 
-	def random_post_tweet
-		coordinate(nil)
-		@client.update("Today's Random City: #{@city}'s weather is #{@forecast.summary} and #{@forecast.temperature} celsius")
-	end
+  def random_post_tweet
+    coordinate(nil)
+    @client.update("Today's Random City: #{@city}'s weather is #{@forecast.summary} and #{@forecast.temperature} celsius")
+  end
 
-	def coordinate(result)
-		if result.nil?
-			random_lat, random_long = Random.new.rand(-90..90) , Random.new.rand(-180..180)
-			while (location = (Geocoder.search("#{random_lat}, #{random_long}"))) == []
-				location = (Geocoder.search("#{random_lat}, #{random_long}"))
-			end		
-			location[0].city.empty? ? @city = 'Unknown City' : @city = location[0].city
-		else
-			location = Geocoder.search(result)
-			@city = result
-		end
-		lat, long = location[0].latitude, location[0].longitude
-		@forecast = ForecastIO.forecast(lat, long, params: { units: 'si' }).currently
-	end
+  private
 
-	private
+  def coordinate(ip_address)
+    if ip_address.nil?
+      random_lat, random_long = Random.new.rand(-90..90) , Random.new.rand(-180..180)
+      while (location = (Geocoder.search("#{random_lat}, #{random_long}"))) == []
+        location = (Geocoder.search("#{random_lat}, #{random_long}"))
+      end   
+      location[0].city.empty? ? @city = 'Unknown City' : @city = location[0].city
+    else
+      location = Geocoder.search(ip_address)
+      @city = ip_address
+    end
+    lat, long = location[0].latitude, location[0].longitude
+    @forecast = ForecastIO.forecast(lat, long, params: { units: 'si' }).currently
+  end
 
-	def twitter_credentials
-		Twitter::REST::Client.new do |config|
-		  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-	    config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-	    config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-	    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-	  end
-	end
+  private
+
+  def twitter_credentials
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+  end
 
 end
